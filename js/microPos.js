56eaa7a918a226bc7723361c09b69cc019e206cf
Camilo Andres Pulido <camilonosdocs@gmail.com>
2016-04-12 20:48:34 -0500

    firt load

---
+++ js/microPos.js
@@ -0,0 +1,26 @@
+
+// constants
+
+var TIME_OUT_CLEAR = 3000;
+
+
+/*
+	loadItem
+
+*/
+function loadItem(){
+
+	// clear bar
+
+	
+
+	setTimeout(clearBar, TIME_OUT_CLEAR)
+
+	return false;
+}
+
+
+function clearBar(){
+
+	$("#codigo").val("");
+}
